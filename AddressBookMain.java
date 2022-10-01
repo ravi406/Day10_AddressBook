d94acc458992b6d9df3e1ce94a93da02f996dad4
ravi406 <ravikamble406@gmail.com>
2022-10-01 22:53:12 +0530

    Welcome To Address Book Program

---
+++ AddressBookMain.java
@@ -0,0 +1,11 @@
+package com.day10.practice;
+
+public class AddressBookMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("******* Welcome To AddressBook Program *******");
+
+    }
+
+}
